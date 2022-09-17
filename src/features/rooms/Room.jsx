014a1bdd445596905512b85ac20e5b30b40218a1
danisanz <daniDNZ@users.noreply.github.com>
2022-09-17 12:47:10 +0200

    Single view page for rooms without images

--- src/features/rooms/Room.jsx
+++ src/features/rooms/Room.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Amenitie, AmenitiesContainer, SingleView } from '../../style/styledComponents';
 import { fetchRoom, selectRoom } from './roomsSlice';
 
 function Room() {
@@ -12,57 +13,87 @@ function Room() {
     dispatch(fetchRoom(id));
   }, []);
 
-  if (!room) {
+  if (!room || Object.keys(room).length === 0) {
     return 'Loading...';
   }
 
   return (
-    <div>
-      <div>
-        Col 1
-        <div>Col 1.1 Img</div>
+    <SingleView>
+      <div className="single-view__container">
+
         <div>
-          Col 1.2
-          <div>
-            <div style={{ display: 'inline-block' }}>
-              <span>
+          <div className="single-view__header">
+            <div>
+              <span className="single-view__title">
                 {`${room.type} ${room.number}`}
               </span>
-              <span style={{ display: 'block' }}>{room.id}</span>
+              <span className="single-view__id">
+                #
+                {room.id}
+              </span>
             </div>
-            <div style={{ display: 'inline-block' }}>...</div>
+            <button type="button" className="single-view__menu">...</button>
+          </div>
+          <hr />
+          <div className="single-view__body">
+            <div>
+              <span className="single-view__body-title">Price</span>
+              <span className="single-view__body-subtitle">
+                {room.price}
+                {' '}
+                €
+              </span>
+            </div>
+            <div>
+              <span className="single-view__body-title">Discount</span>
+              <span className="single-view__body-subtitle">
+                {room.discount}
+                {' '}
+                %
+              </span>
+            </div>
+            <div>
+              <span className="single-view__body-title">Offer</span>
+              <span className="single-view__body-subtitle">
+                {room.discount > 0 ? 'Yes' : 'No'}
+              </span>
+            </div>
+          </div>
+          <div>
+            <span className="single-view__body-title">Amenities</span>
+            <AmenitiesContainer>
+              {room.amenities.map((amenitie) => (
+                <Amenitie key={`am-${amenitie}`}>
+                  {amenitie}
+                </Amenitie>
+              ))}
+            </AmenitiesContainer>
           </div>
         </div>
-        <hr />
         <div>
+          {/* <div>
+            Carousel
+            {' '}
+            {room.photos}
+            <button type="button">prev</button>
+            <button type="button">next</button>
+          </div> */}
           <div>
-            <span>Price</span>
-            <span style={{ display: 'block' }}>
-              {room.price}
-              {' '}
-              €
+            <span className="single-view__body-title">Description</span>
+            <span className="single-view__body-text">
+              {room.description}
             </span>
           </div>
-        </div>
-        <div>
-          <span>Amenities</span>
           <div>
-            {room.amenities}
+            <span className="single-view__body-title">Cancellation</span>
+            <span className="single-view__body-text">
+              {room.cancellation}
+            </span>
           </div>
+          <span style={{ display: 'block' }}>{room.description}</span>
         </div>
       </div>
-      <div>
-        Col 2
-        <div>
-          Carousel
-          {' '}
-          {room.photos}
-          <button type="button">prev</button>
-          <button type="button">next</button>
-        </div>
-        <span style={{ display: 'block' }}>{room.description}</span>
-      </div>
-    </div>
+    </SingleView>
   );
 }
 
--- src/style/styledComponents.js
+++ src/style/styledComponents.js
@@ -41,7 +41,21 @@ const Subtitle = styled.h2`
   margin-bottom: 40px;
 `;
 
+const AmenitiesContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 10px;
+`;
+
 const Amenitie = styled.div`
+  width: fit-content;
+  background-color: ${colors.lightGreen};
+  color: ${colors.hardGreen};
+  font-size: 16px;
+  border-radius: 12px;
+
+  padding: 20px 25px;
+
 `;
 
 const Row = styled.div`
@@ -49,6 +63,86 @@ const Row = styled.div`
   flex-flow: row wrap;
   justify-content: space-between;
 `;
+
+const SingleView = styled.div`
+  background-color: white;
+  box-shadow: 0px 4px 4px #00000005;
+  border-radius: 20px;
+
+  padding: 40px;
+
+  & .single-view {
+
+    &__container {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 30px;
+    }
+
+    &__header {
+      border-bottom: 2px solid ${colors.bgGray};
+
+      margin-bottom: 30px;
+
+      display: flex;
+      justify-content: space-between;
+    }
+    
+    &__title {
+      font-size: 30px;
+      font-weight: 600;
+
+      margin-bottom: 10px;
+
+      display: block;
+    }
+
+    &__id {
+      color: ${colors.green};
+
+      margin-bottom: 30px;
+      display: block;
+    }
+
+    &__menu {
+      height: 20px;
+      width: 20px;
+
+      display: inline-block;
+    }
+
+    &__body {
+
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+
+      &-title {
+        font-size: 14px;
+        color: ${colors.gray};
+
+        margin-bottom: 10px;
+  
+        display: block;
+      }
+  
+      &-subtitle {
+        font-size: 24px;
+
+        margin-bottom: 30px;
+  
+        display: block;
+      }
+
+      &-text {
+        margin-bottom: 30px;
+
+        display: block;
+      }
+    }
+  }
+
+
+`;
 export {
-  Logo, Button, Amenitie, Subtitle, Row,
+  Logo, Button, Amenitie, AmenitiesContainer, Subtitle, Row, SingleView,
 };
