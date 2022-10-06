af49340c8d2b0bd3b685cf2e8ce70306870d20f8
danisanz <daniDNZ@users.noreply.github.com>
2022-10-06 17:21:09 +0200

    dateBuilder function

--- src/assets/functions/index.js
+++ src/assets/functions/index.js
@@ -4,4 +4,9 @@ const checkIsNotAButton = (e, navigate) => {
   }
 };
 
-export default checkIsNotAButton;
+const dateBuilder = (data) => {
+  const date = new Date(data);
+  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+};
+
+export { checkIsNotAButton, dateBuilder };
